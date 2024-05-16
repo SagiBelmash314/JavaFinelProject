5da9b9c142a0c0040f969d60dd053e1025ece0ce
Sagi <sagibelmash@gmail.com>
2024-05-16 11:54:00 +0300

    Finished question1

--- Main.java
+++ Main.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner reader = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-        String[] sellers = new String[0], buyers = new String[0];
-        int sCount = 0, bCount = 0, input;
-        String seller, buyer;
+        String[] sellers = new String[0];
+        String[] buyers = new String[0];
+        int sCount = 0;
+        int bCount = 0;
+        int input;
 
         do {
             System.out.println("""
@@ -28,62 +30,88 @@ public class Main {
             input = reader.nextInt();
             switch (input) {
                 case 1: // Add seller
-                    System.out.println("Please enter the name of the seller: ");
-                    do {
-                        String sName = reader.next();
-                        if (!inList(sellers, sName)) {
-                            sellers = addToList(sellers, sName, sCount);
-                            sCount++;
-                            break;
-                        }
-                        else {
-                            System.out.println("Seller is already in the list, please enter another seller: ");
-                        }
-                    } while (true);
+                    sellers = addSeller(sellers, sCount);
+                    sCount++;
                     break;
                 case 2: // Add buyer
-                    System.out.println("Please enter the name of the buyer: ");
-                    do {
-                        String bName = reader.next();
-                        if (inList(buyers, bName)) {
-                            System.out.println("Buyer is already in the list, please enter another buyer: ");
-                        }
-                        else {
-                            buyers = addToList(buyers, bName, bCount);
-                            bCount++;
-                            break;
-                        }
-                    } while (true);
+                    buyers = addBuyer(buyers, bCount);
+                    bCount++;
                     break;
                 case 3: // Add product to seller
-                    seller = chooseSeller(sellers);
-                    System.out.println("Please enter the name of the product: ");
-                    String product = reader.next();
-                    System.out.println("Please enter the price of the product: ");
-                    float price = reader.nextFloat();
-                    System.out.println("Please enter the category of the product: ");
-                    String category = reader.next();
+                    addProductSeller(sellers);
                     break;
                 case 4: // Add product to buyer
-                    buyer = chooseBuyer(buyers);
-                    seller = chooseSeller(sellers);
+                    addProductBuyer(buyers, sellers);
                     break;
                 case 5: // Order payment for buyer
-                    buyer = chooseBuyer(buyers);
+                    orderPayment(buyers);
                     break;
                 case 6: // Show details of all buyers
-                    for (String b: buyers) {
-                        System.out.println(b);
-                    }
+                    printDetails(buyers);
                     break;
                 case 7: // Show details of all sellers
-                    for (String s: sellers) {
-                        System.out.println(s);
-                    }
+                    printDetails(sellers);
                     break;
             }
         } while (input != 0);
         reader.close();
+    }
+
+    private static void addProductBuyer(String[] buyers, String[] sellers) {
+        String buyer = chooseBuyer(buyers);
+        String seller = chooseSeller(sellers);
+    }
+
+    private static void orderPayment(String[] buyers) {
+        String buyer = chooseBuyer(buyers);
+    }
+
+    private static String[] addSeller(String[] sellers, int sCount) {
+        System.out.println("Please enter the name of the seller: ");
+        do {
+            String sName = reader.next();
+            if (!inList(sellers, sName)) {
+                sellers = addToList(sellers, sName, sCount);
+                break;
+            }
+            else {
+                System.out.println("Seller is already in the list, please enter another seller: ");
+            }
+        } while (true);
+        return sellers;
+    }
+
+    private static String[] addBuyer(String[] buyers, int bCount) {
+        System.out.println("Please enter the name of the buyer: ");
+        do {
+            String bName = reader.next();
+            if (inList(buyers, bName)) {
+                System.out.println("Buyer is already in the list, please enter another buyer: ");
+            }
+            else {
+                buyers = addToList(buyers, bName, bCount);
+                bCount++;
+                break;
+            }
+        } while (true);
+        return buyers;
+    }
+
+    private static void addProductSeller(String[] sellers) {
+        String seller = chooseSeller(sellers);
+        System.out.println("Please enter the name of the product: ");
+        String product = reader.next();
+        System.out.println("Please enter the price of the product: ");
+        float price = reader.nextFloat();
+        System.out.println("Please enter the category of the product: ");
+        String category = reader.next();
+    }
+
+    private static void printDetails(String[] ls) {
+        for (String i: ls) {
+            System.out.println(i);
+        }
+
     }
 
     private static String[] addToList(String[] ls, String item, int count) {
