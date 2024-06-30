9a96eac962828766d2157e8208f066bafcb769fc
Sagi <sagibelmash@gmail.com>
2024-06-30 20:13:35 +0300

    Added Serial num to Product and created Category class and implemented it fully.

--- Product.java
+++ Product.java
@@ -1,15 +1,22 @@
 public class Product {
     private final String name;
     private final float price;
+    private final int serialNum;
+    private static int numOfProducts = 0;
+    private final Category category;
 
-    public Product(String name, float price) {
+    public Product(String name, float price, Category category) {
         this.name = name;
         this.price = price;
+        this.serialNum = numOfProducts++;
+        this.category = category;
     }
 
     public Product(Product other) {
         this.name = other.name;
         this.price = other.price;
+        this.serialNum = other.serialNum;
+        this.category = other.category;
     }
 
     public String getName() {
@@ -20,8 +27,17 @@ public class Product {
         return price;
     }
 
+    public int getSerialNum() {return serialNum;}
+
+    public Category getCategory() {
+        return category;
+    }
+
     @Override
     public String toString() {
-        return name + " ---> " + price + " ILS";
+        StringBuilder sb = new StringBuilder(name);
+        sb.append(" (").append(category).append(") [")
+                .append(serialNum).append("] ---> ").append(price).append(" ILS");
+        return sb.toString();
     }
 }
